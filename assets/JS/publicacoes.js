08804da18db1bf55158460a00067d621b611c560
Ester Prazim <ester.prazim@ufpe.br>
2019-05-26 12:05:17 -0300

    Ajustar mobile, iniciar integração

--- assets/JS/publicacoes.js
+++ assets/JS/publicacoes.js
@@ -1,44 +1,44 @@
 $(document).ready(function(){
-    $('.multiple-items').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        speed: 200,
-        prevArrow: document.querySelector('#playButtonBack'),
-        nextArrow: document.querySelector('#playButtonNext'),
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 700,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-                arrows: false,
-              }
-            },
-        ]
-      });
-  });
+    $('.multipleItems').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed: 200,
+      prevArrow: document.querySelector('#playButtonBack'),
+      nextArrow: document.querySelector('#playButtonNext'),
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            arrows: false,
+          }
+        },
+      ]
+    });
+});
       
 
       
